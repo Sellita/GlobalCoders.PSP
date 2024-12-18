6f69b9c13474fd0a65b98ead48e13c76569d8b3e
Santi-ramirez-practicas <santiago.ramirez-practicas@satocan.com>
2024-12-18 17:59:13 +0000

    Organization service updated.

--- src/GlobalCoders.PSP.Frontend/Frontend/src/services/org.service.ts
+++ src/GlobalCoders.PSP.Frontend/Frontend/src/services/org.service.ts
@@ -1,27 +1,44 @@
 
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { OnInit } from '@angular/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrgService {
 
+  token: string = localStorage.getItem('accessToken') || '';
+  headers: HttpHeaders = new HttpHeaders(
+    {
+      Authorization: `Bearer ${this.token}`, // Incluye el token en el encabezado de autorización
+    }
+  );
+
   constructor(private http: HttpClient) {}
 
   getOrganizations(){
-    const token = localStorage.getItem('accessToken');
-    console.log(token);
-    const headers = new HttpHeaders({
-      Authorization: `Bearer ${token}`, // Incluye el token en el encabezado de autorización
-    });
-
     const body = {
         "page": 1,
         "itemsPerPage": 100,
     };
+    return this.http.post('http://localhost:9001/Organization/All', body, { headers: this.headers });
+  }
 
-    return this.http.post('http://localhost:9001/Organization/All',body, { headers });
+  createOrganization(organization: any){
+    return this.http.post('http://localhost:9001/Organization/Create', organization, { headers: this.headers });
+  }
+
+  updateOrganization(organization: any){
+    return this.http.put('http://localhost:9001/Organization/Update', organization,  { headers: this.headers });
+  }
+
+  deleteOrganization(id: string){
+    return this.http.delete('http://localhost:9001/Organization/Delete/'+id, { headers: this.headers });
+  }
+
+  getOrganization(id: string){
+    return this.http.get('http://localhost:9001/Organization/Id/'+id, { headers: this.headers });
   }
 
   
